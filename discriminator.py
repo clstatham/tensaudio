fixed loss calculation code

discriminator.py
@@ -6,44 +6,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 from helper import *
 from global_constants import *
-from hilbert import inverse_hilbert
+from hilbert import *
 import os
 import inspect
 
 class DPAM_Discriminator(nn.Module):
-    def __init__(self, n_layers=N_DIS_LAYERS, reuse=False, norm_type="SBN",
-                ksz=DIS_KERNEL_SIZE, base_channels=N_DIS_CHANNELS, blk_channels=4, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(DPAM_Discriminator, self).__init__(*args, **kwargs)
        
         self.loss = nn.BCELoss()
 
-        self.ln_layers = []
-        self.base_channels = base_channels
-        self.blk_channels = blk_channels
-        self.ksz = ksz
-        #self.ndf = 1 + N_FFT // 2
+        self.n_layers = N_DIS_LAYERS
+        self.ksz = DIS_KERNEL_SIZE
         self.ndf = 2
-        # for i in range(n_layers):
-        #     self.ln_layers.append(nn.ReLU())
-        #     self.ln_layers.append(nn.Conv2d(self.layer_channels_by_index(i), self.layer_channels_by_index(i+1), ksz, stride=1))
-        #     #self.ln_layers.append(nn.Dropout(1.0))
-        # self.ln_layers = nn.ModuleList(self.ln_layers)
 
         self.net = []
-        self.net.append(nn.Conv2d(self.ndf, self.ndf*2, self.ksz, 2, 1, bias=False))
-        self.net.append(nn.BatchNorm2d(self.ndf*2))
-        self.net.append(nn.LeakyReLU(0.2, inplace=True))
+        i = 0
+        for _ in range(self.n_layers):
+            c = 2**i
+            n = 2**(i+1)
+            self.net.append(nn.Conv2d(self.ndf*c, self.ndf*n, self.ksz, 2, 1, bias=False))
+            self.net.append(nn.BatchNorm2d(self.ndf*n))
+            self.net.append(nn.LeakyReLU(0.2, inplace=True))
+            i += 1
 
-        self.net.append(nn.Conv2d(self.ndf*2, self.ndf*4, self.ksz, 2, 1, bias=False))
-        self.net.append(nn.BatchNorm2d(self.ndf*4))
-        self.net.append(nn.LeakyReLU(0.2, inplace=True))
-
-        self.net.append(nn.Conv2d(self.ndf*4, self.ndf*8, self.ksz, 2, 1, bias=False))
-        self.net.append(nn.BatchNorm2d(self.ndf*8))
-        self.net.append(nn.LeakyReLU(0.2, inplace=True))
-
-        self.net.append(nn.Conv2d(self.ndf*8, 1, self.ksz, 1, 0, bias=False))
+        self.net.append(nn.Conv2d(self.ndf*(2**i), 1, self.ksz, 1, 0, bias=False))
         self.net.append(nn.Sigmoid())
+
         self.net = nn.ModuleList(self.net)
 
         # self.ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=self.optimizer, net=self)
@@ -51,12 +40,14 @@ class DPAM_Discriminator(nn.Module):
         
         # if self.manager.latest_checkpoint:
         #     self.ckpt.restore(self.manager.latest_checkpoint)
-        #     print("Restored DPAM from {}".format(self.manager.latest_checkpoint))
+        #     cprint("Restored DPAM from {}".format(self.manager.latest_checkpoint))
         # else:
-        #     print("Initializing DPAM from scratch.")
+        #     cprint("Initializing DPAM from scratch.")
     
-    def criterion(self, ex, op):
-        return self.loss(torch.unsqueeze(torch.mean(op), 0), torch.sigmoid(torch.unsqueeze(torch.mean(ex), 0)))
+    def criterion(self, label, output):
+        l = self.loss(torch.unsqueeze(output, 0), torch.unsqueeze(label, 0))
+        l = F.relu(l)
+        return l
 
     def layer_channels_by_index(self, i):
         return np.max((1, self.base_channels * (i)))
@@ -79,15 +70,22 @@ class DPAM_Discriminator(nn.Module):
         return loss_result
 
     def forward(self, input1):
-        real, imag = STFTWithGradients.apply(input1, N_FFT).cuda()
-        stft1 = torch.stack((real, imag))
-        stft1 = torch.unsqueeze(stft1, 0)
-        #input1 = prep_data_for_batch_operation(input1, None, input1.shape[0], 1).float()
-        #print("In discriminator layer #", 0)
-        out = self.net[0](stft1)
+        if DIS_MODE == 0:
+            amp, phase = HilbertWithGradients.apply(input1).detach().cuda()
+            hilb = torch.stack((amp, phase))
+            hilb = torch.unsqueeze(hilb, 0)
+            hilb = torch.unsqueeze(hilb, 3)
+            out = self.net[0](hilb)
+        elif DIS_MODE == 1:
+            real, imag = STFTWithGradients.apply(input1).detach().cuda()
+            stft1 = torch.stack((real, imag))
+            stft1 = torch.unsqueeze(stft1, 0)
+            out = self.net[0](stft1)
+        
         i = 1
         for layer in self.net[1:]:
-            #print("In discriminator layer #", i)
+            #cprint("In discriminator layer #", i)
             out = layer(out)
             i += 1
+        #out = torch.mean(out)
         return out

generator.py
@@ -9,11 +9,11 @@ from hilbert import *
 from global_constants import *
 
 # foo = np.arange(0, N_BATCHES*2)
-# print(foo)
+# cprint(foo)
 # foo = prep_audio_for_batch_operation(foo)
-# print(foo)
+# cprint(foo)
 # foo = flatten_audio(foo)
-# print(foo)
+# cprint(foo)
 
 class TA_Generator(nn.Module):
     def __init__(self, **kwargs):
@@ -27,28 +27,16 @@ class TA_Generator(nn.Module):
             raise NotImplementedError("TODO: implement RNN/Hilbert mode")
         if GEN_MODE == 1:
             #TODO: port RNN/Audio code to pytorch
-            raise NotImplementedError("TODO: implement RNN/Hilbert mode")
-            self.audio_rnns = []
-            self.audio_rnns.append(nn.LSTM(N_TIMESTEPS_PER_KERNEL, return_state=True, stateful=True, time_major=False, go_backwards=False, name="f_audio_rnn_0"))
-            for i in range(N_RNN_LAYERS-1):
-                layer_f = LSTM(N_TIMESTEPS_PER_KERNEL, return_state=True, stateful=True, time_major=False, go_backwards=False, name="f_audio_rnn_"+str(i+1))
-                #layer_b = SimpleRNN(N_UNITS*2, return_state=True, stateful=True, time_major=True, go_backwards=True, name="b_audio_rnn_"+str(l))
-                #self.audio_rnns.append(Bidirectional(layer_f, backward_layer=layer_b, merge_mode='ave'))
-                self.audio_rnns.append(layer_f)
+            raise NotImplementedError("TODO: implement RNN/Audio mode")
         elif GEN_MODE == 2:
             self.create_dense_net(hilb_mode=True)
         elif GEN_MODE == 3:
             self.create_dense_net(hilb_mode=False)
-        
 
-        # if self.manager.latest_checkpoint:
-        #     self.ckpt.restore(self.manager.latest_checkpoint)
-        #     print("Generator Restored from {}".format(self.manager.latest_checkpoint))
-        # else:
-        #     print("Generator Initializing from scratch.")
-
-    def criterion(self, ex, op):
-        return self.loss(torch.unsqueeze(torch.mean(op), 0), torch.unsqueeze(torch.mean(ex), 0))
+    def criterion(self, label, output):
+        l = self.loss(torch.unsqueeze(output, 0), torch.unsqueeze(label, 0))
+        l = F.relu(l)
+        return l
     
     def create_dense_net(self, hilb_mode=False):
         self.preprocess_layers = []
@@ -62,9 +50,9 @@ class TA_Generator(nn.Module):
         self.output_padding = 0
         self.stride = 1
 
-        v_print("*-"*39 + "*")
+        v_cprint("*-"*39 + "*")
 
-        v_print("Creating ReLU layer.")
+        v_cprint("Creating ReLU layer.")
         self.initial_layer = nn.ReLU().cuda()
 
         self.n_preprocessing_indices = 1
@@ -74,32 +62,33 @@ class TA_Generator(nn.Module):
         Lin = TOTAL_SAMPLES_IN
         while Lout*N_BATCHES*N_CHANNELS*4 < DESIRED_PROCESS_UNITS:
             Lout=self.n_preprocessing_indices*(Lin-1)*self.stride-2*self.padding*self.dilation*(self.kernel_size-1)*self.output_padding+1
-            vv_print("Creating deconvolution layer", Lin, ">", Lout, "ksz =", self.kernel_size, "stride =", self.stride, "total =", N_BATCHES*N_CHANNELS*Lout)
+            vv_cprint("Creating deconvolution layer", Lin, ">", Lout, "ksz =", self.kernel_size, "stride =", self.stride, "total =", N_BATCHES*N_CHANNELS*Lout)
             self.preprocess_layers.append(nn.ConvTranspose1d(Lin, Lout, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding, output_padding=self.output_padding, dilation=self.dilation).cuda())
             Lin  = Lout
             self.n_preprocessing_indices += 1
         self.n_preprocessing_indices -= 1
-        v_print("Created", self.n_preprocessing_indices+1, "preprocessing layers.")
+        v_cprint("Created", self.n_preprocessing_indices+1, "preprocessing layers.")
 
-        self.n_process_units = Lout*N_BATCHES*N_CHANNELS*4
-        vv_print("Creating", N_PROCESS_LAYERS, "Linear layers", self.n_process_units, ">", self.n_process_units)
+        self.n_process_units = Lout*N_BATCHES*N_CHANNELS*(TOTAL_SAMPLES_IN//2)
+        vv_cprint("Creating", N_PROCESS_LAYERS, "Linear layers", self.n_process_units, ">", self.n_process_units)
         for i in range(N_PROCESS_LAYERS):
             self.process_layers.append(nn.Linear(self.n_process_units, self.n_process_units).cuda())
-        print("Created", len(self.process_layers), "processing layers.")
+        v_cprint("Created", len(self.process_layers), "processing layers.")
 
         Lin = self.n_process_units
         Lout = 0
-        while Lout*N_BATCHES*N_CHANNELS < TOTAL_SAMPLES_OUT:
+        while Lout*N_BATCHES*N_CHANNELS*self.kernel_size < TOTAL_SAMPLES_OUT:
             Lout=self.n_postprocessing_indices*(Lin-1)*self.stride-2*self.padding*self.dilation*(self.kernel_size-1)*self.output_padding+1
-            vv_print("Creating deconvolution layer", Lin, ">", Lout, "ksz =", self.kernel_size, "stride =", self.stride, "total =", N_BATCHES*N_CHANNELS*Lout)
+            vv_cprint("Creating deconvolution layer", Lin, ">", Lout, "ksz =", self.kernel_size, "stride =", self.stride, "total =", N_BATCHES*N_CHANNELS*Lout)
             self.postprocess_layers.append(nn.ConvTranspose1d(Lin, Lout, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding, output_padding=self.output_padding, dilation=self.dilation).cuda())
             Lin  = Lout
             self.n_postprocessing_indices += 1
         self.n_postprocessing_indices -= 1
-        v_print("Created", self.n_postprocessing_indices+1, "postprocessing layers.")
+        v_cprint("Created", self.n_postprocessing_indices+1, "postprocessing layers.")
 
-        print("Creating ReLU Layer.")
-        self.final_layer = nn.ReLU().cuda()
+        v_cprint("Creating Linear Layer.")
+        #self.final_layer = nn.ReLU().cuda()
+        self.final_layer = nn.Linear(Lout, TOTAL_SAMPLES_OUT)
         
         self.preprocess_layers = nn.ModuleList(self.preprocess_layers).cuda()
         self.process_layers = nn.ModuleList(self.process_layers).cuda()
@@ -122,24 +111,24 @@ class TA_Generator(nn.Module):
             f_state1 = self.rnn_state
         #b_state1 = b_state1[0]
 
-        vv_print("|} Ready for launch! Going to the net now, wheeee!")
+        vv_cprint("|} Ready for launch! Going to the net now, wheeee!")
 
         audio, f_state1, f_state2 = self.audio_rnns[0](audio, initial_state=(f_state1, f_state2), training=training)
-        vv_print("|} Layer 1 done.")
+        vv_cprint("|} Layer 1 done.")
         for i in range(N_RNN_LAYERS-1):
             audio = prep_data_for_batch_operation(audio)
             #audio = tf.expand_dims(audio, axis=0)
             audio, f_state1, f_state2 = self.audio_rnns[i+1](audio, initial_state=(f_state1, f_state2), training=training)
-            vv_print("|}} Layer", i+2, "done.")
+            vv_cprint("|}} Layer", i+2, "done.")
 
-        vv_print("|} Whew... Made it out of the net alive!")
+        vv_cprint("|} Whew... Made it out of the net alive!")
 
         #self.rnn_state = (f_state1, f_state2)
         audio = K.flatten(audio)
         return audio
 
     def run_dense_net(self, data, hilb_mode=False):
-        vv_print("|}} Initial data.shape:", data.shape)
+        vv_cprint("|}} Initial data.shape:", data.shape)
         
         data = self.initial_layer(data.float())
 
@@ -147,25 +136,25 @@ class TA_Generator(nn.Module):
         Lout = (Lin-1)*self.stride-2*self.padding*self.dilation*(self.kernel_size-1)*self.output_padding+1
         data = prep_data_for_batch_operation(data, None, TOTAL_SAMPLES_IN, 1)
 
-        vv_print("|}} Initial layer done.")
-        vv_print("|}} data.shape:", data.shape)
+        vv_cprint("|}} Initial layer done.")
+        vv_cprint("|}} data.shape:", data.shape)
         i = 0
         for layer in self.preprocess_layers:
             data = layer(data.float())
-            vv_print("|}} Pre-process layer", i+1, "done.")
-            vv_print("|}} data.shape:", data.shape)
+            vv_cprint("|}} Pre-process layer", i+1, "done.")
+            vv_cprint("|}} data.shape:", data.shape)
             i += 1
         
         data = torch.squeeze(data)
         data = data.flatten()
-        vv_print("|}} data.shape going into process layers:", data.shape)
+        vv_cprint("|}} data.shape going into process layers:", data.shape)
         for i in range(len(self.process_layers)):
             data = self.process_layers[i](data)
             data = data.flatten()
-            #vv_print("|}} Process layer", i+1, "done.")
-            #vv_print("|}} data.shape:", data.shape)
-        vv_print("|}} Process layers done.")
-        vv_print("|}} data.shape:", data.shape)
+            #vv_cprint("|}} Process layer", i+1, "done.")
+            #vv_cprint("|}} data.shape:", data.shape)
+        vv_cprint("|}} Process layers done.")
+        vv_cprint("|}} data.shape:", data.shape)
 
         Lin = self.n_process_units
         Lout = (Lin-1)*self.stride-2*self.padding*self.dilation*(self.kernel_size-1)*self.output_padding+1
@@ -174,33 +163,35 @@ class TA_Generator(nn.Module):
         i = 1
         for layer in self.postprocess_layers:
             data = layer(data)
-            vv_print("|}} Post-process layer", i, "done.")
-            vv_print("|}} data.shape:", data.shape)
+            vv_cprint("|}} Post-process layer", i, "done.")
+            vv_cprint("|}} data.shape:", data.shape)
             i += 1
         
+        data = data.flatten()
+        #print(data.shape)
         data = self.final_layer(data)
         data = data.flatten()
-        vv_print("|}} Final layer done.")
-        vv_print("|}} Final data.shape:", data.shape)
+        vv_cprint("|}} Final layer done.")
+        vv_cprint("|}} Final data.shape:", data.shape)
         return data
 
     def gen_dense_hilb(self, hilb):
         assert (GEN_MODE == 2)
         hilb = torch.tensor(stack_hilb(hilb)).cuda()
         hilb = prep_data_for_batch_operation(hilb, N_BATCHES, N_CHANNELS, None)
-        vv_print("|} Ready for launch! Going to the net now, wheeee!")
+        vv_cprint("|} Ready for launch! Going to the net now, wheeee!")
         hilb = self.run_dense_net(hilb, hilb_mode=True)
-        vv_print("|} Whew... Made it out of the net alive!")
+        vv_cprint("|} Whew... Made it out of the net alive!")
         #hilb = tf.squeeze(hilb)
         #hilb = hilb[-1, :, :]
-        vv_print("|}} hilb.shape:", hilb.shape)
+        vv_cprint("|}} hilb.shape:", hilb.shape)
         hilb = torch.flatten(hilb)
         hilb, error = ensure_size(hilb, N_CHANNELS*TOTAL_SAMPLES_IN)
         hilb = [[hilb[i] for i in range(len(hilb)) if i%2==0], [hilb[i+1] for i in range(len(hilb)) if i%2==0]]
         if error > 0:
-            vv_print("|} Truncated output audio by", error//N_CHANNELS, "samples.")
+            vv_cprint("|} Truncated output audio by", error//N_CHANNELS, "samples.")
         else:
-            vv_print("|} Padded output audio by", -error//N_CHANNELS, "samples.")
+            vv_cprint("|} Padded output audio by", -error//N_CHANNELS, "samples.")
         #assert(len(hilb) == 2 and len(hilb[0] == TOTAL_SAMPLES_IN))
         return hilb[0], hilb[1]
 
@@ -213,9 +204,9 @@ class TA_Generator(nn.Module):
         orig_shape = audio.shape
         
         audio = prep_data_for_batch_operation(audio, N_BATCHES, N_CHANNELS, None)
-        vv_print("|} Ready for launch! Going to the net now, wheeee!")
+        vv_cprint("|} Ready for launch! Going to the net now, wheeee!")
         audio = self.run_dense_net(audio, hilb_mode=False)
-        vv_print("|} Whew... Made it out of the net alive!")
+        vv_cprint("|} Whew... Made it out of the net alive!")
         
         audio = torch.squeeze(audio)
         #audio = audio[:, -1, :] # the last
@@ -223,9 +214,9 @@ class TA_Generator(nn.Module):
         audio = torch.flatten(audio)
         audio, error = ensure_size(audio, TOTAL_SAMPLES_OUT)
         if error > 0:
-            vv_print("|} Truncated output audio by", error, "samples.")
+            vv_cprint("|} Truncated output audio by", error, "samples.")
         else:
-            vv_print("|} Padded output audio by", -error, "samples.")
+            vv_cprint("|} Padded output audio by", -error, "samples.")
         return audio
 
     def gen_fn(self, inputs):

helper.py
@@ -115,12 +115,12 @@ def periodic_hann(window_length):
     A 1D np.array containing the periodic hann window.
   """
   return 0.5 - (0.5 * np.cos(2 * np.pi / window_length *
-                             np.arange(window_length)))
+                            np.arange(window_length)))
 
 
 def stft_magnitude(signal, fft_length,
-                   hop_length=None,
-                   window_length=None):
+                  hop_length=None,
+                  window_length=None):
   """Calculate the short-time Fourier transform magnitude.
   Args:
     signal: 1D np.array of the input time-domain signal.
@@ -198,10 +198,10 @@ def spectrogram_to_mel_matrix(num_mel_bins=64,
     raise ValueError("lower_edge_hertz %.1f must be >= 0" % lower_edge_hertz)
   if lower_edge_hertz >= upper_edge_hertz:
     raise ValueError("lower_edge_hertz %.1f >= upper_edge_hertz %.1f" %
-                     (lower_edge_hertz, upper_edge_hertz))
+                    (lower_edge_hertz, upper_edge_hertz))
   if upper_edge_hertz > nyquist_hertz:
     raise ValueError("upper_edge_hertz %.1f is greater than Nyquist %.1f" %
-                     (upper_edge_hertz, nyquist_hertz))
+                    (upper_edge_hertz, nyquist_hertz))
   spectrogram_bins_hertz = np.linspace(0.0, nyquist_hertz, num_spectrogram_bins)
   spectrogram_bins_mel = hertz_to_mel(spectrogram_bins_hertz)
   # The i'th mel band (starting from i=1) has center frequency
@@ -209,7 +209,7 @@ def spectrogram_to_mel_matrix(num_mel_bins=64,
   # band_edges_mel[i+1].  Thus, we need num_mel_bins + 2 values in
   # the band_edges_mel arrays.
   band_edges_mel = np.linspace(hertz_to_mel(lower_edge_hertz),
-                               hertz_to_mel(upper_edge_hertz), num_mel_bins + 2)
+                              hertz_to_mel(upper_edge_hertz), num_mel_bins + 2)
   # Matrix to post-multiply feature arrays whose rows are num_spectrogram_bins
   # of spectrogram values.
   mel_weights_matrix = np.empty((num_spectrogram_bins, num_mel_bins))
@@ -218,9 +218,9 @@ def spectrogram_to_mel_matrix(num_mel_bins=64,
     # Calculate lower and upper slopes for every spectrogram bin.
     # Line segments are linear in the *mel* domain, not hertz.
     lower_slope = ((spectrogram_bins_mel - lower_edge_mel) /
-                   (center_mel - lower_edge_mel))
+                  (center_mel - lower_edge_mel))
     upper_slope = ((upper_edge_mel - spectrogram_bins_mel) /
-                   (upper_edge_mel - center_mel))
+                  (upper_edge_mel - center_mel))
     # .. then intersect them with each other and zero.
     mel_weights_matrix[:, i] = np.maximum(0.0, np.minimum(lower_slope,
                                                           upper_slope))
@@ -283,7 +283,7 @@ def voc_ap(rec, prec, use_07_metric=True):
         # first append sentinel values at the end
         mrec = np.concatenate(([0.], rec, [1.]))
         mpre = np.concatenate(([0.], prec, [0.]))
-        #print('abc')
+        #cprint('abc')
         # compute the precision envelope
         for i in range(mpre.size - 1, 0, -1):
             mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
@@ -321,13 +321,19 @@ def ensure_size(t, size, channels=1):
     t = t.flatten()
     delta = t.shape[0] - size
     if delta < 0:
-      t = torch.cat((torch.tensor(t), torch.zeros(-delta).cuda()))
+      if type(t) == torch.Tensor:
+        t = torch.cat((t, torch.zeros(-delta).cuda()))
+      else:
+        t = np.concatenate((t, np.zeros(-delta)))
     elif delta > 0:
       t = t[:-delta]
   else:
     delta = t.shape[1]*channels - size
     if delta < 0:
-      t = torch.tensor([torch.cat(t[i], torch.zeros(t[i].shape[0])) for i in range(channels)]).cuda()
+      if type(t) == torch.Tensor:
+        t = torch.tensor([torch.cat(t[i], torch.zeros(t[i].shape[0])) for i in range(channels)]).cuda()
+      else:
+        t = [np.concatenate(t[i], np.zeros(len(t[i]))) for i in range(channels)]
     elif delta > 0:
       t = t[:][:-delta]
   return t, delta
@@ -370,15 +376,42 @@ def write_normalized_audio_to_disk(a, fn):
 
 class STFTWithGradients(Function):
     @staticmethod
-    def forward(ctx, p, n_fft):
+    def forward(ctx, p):
         p_ = p.detach().cpu().numpy()
-        result = librosa.stft(p_, n_fft=n_fft)
+        result = librosa.stft(p_, n_fft=N_FFT)
         return p.new((np.real(result), np.imag(result)))
 
     @staticmethod
-    def backward(ctx, grad_output, n_fft):
+    def backward(ctx, grad_output):
         if grad_output is None:
-            return None, None
+          return None, None
         numpy_go = grad_output.cpu().numpy()
-        result = librosa.stft(numpy_go, n_fft=n_fft)
+        #print(numpy_go.shape)
+        data = []
+        for i in range(len(numpy_go[0])):
+          d = []
+          for j in range(len(numpy_go[0][i])):
+            d.append(np.complex(numpy_go[0][i][j], numpy_go[1][i][j]))
+          data.append(d)
+        data = np.array(data)
+        orig_size = data.size//2
+        #print(orig_size)
+        result = librosa.istft(data)
+        #print(result.shape)
+        result, _ = ensure_size(result, TOTAL_SAMPLES_OUT, channels=1)
+        return grad_output.new(result)
+
+class InverseSTFTWithGradients(Function):
+    @staticmethod
+    def forward(ctx, p):
+        p_ = p.detach().cpu().numpy()
+        result = librosa.istft(p_)
+        return p.new((np.real(result), np.imag(result)))
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if grad_output is None:
+          return None, None
+        numpy_go = grad_output.cpu().numpy()
+        result = librosa.stft(numpy_go, n_fft=N_FFT)
         return grad_output.new((np.real(result), np.imag(result)))

hilbert.py
@@ -13,42 +13,8 @@ def spectral_convolution(in1, in2):
     conv = np.multiply(spec1, spec2)
     return np.real(np.fft.ifft(conv))
 
-class InstFreq(Function):
-    @staticmethod
-    def forward(ctx, p):
-        pp = p.detach().cpu().numpy()
-        result = np.diff(pp) / (2.0*np.pi) * len(pp) # hehe
-        return p.new(result)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        if grad_output is None:
-            return None, None
-        numpy_go = grad_output.cpu().numpy()
-        result = np.diff(numpy_go) / (2.0*np.pi) * len(numpy_go)
-        return grad_output.new(result)
-
-def inst_freq(inst_phase):
-    return InstFreq.apply(inst_phase)
-
-# def inverse_hilbert_cos(amplitude_envelope, instantaneous_phase):
-#     T = len(instantaneous_phase)
-
-#     sp = np.fft.fft(instantaneous_phase)
-#     freq = np.fft.fftfreq(instantaneous_phase.shape[-1])
-#     cos = []
-#     for i in range(len(freq)):
-#         cos.append(amplitude_envelope[i] * np.sum((sp[-i]+sp[i]).real/(2*T) * np.cos(2.*np.pi*freq[i] + instantaneous_phase[i])))
-#     return cos
-# def inverse_hilbert_sin(amplitude_envelope, instantaneous_phase):
-#     T = len(instantaneous_phase)
-
-#     sp = np.fft.fft(instantaneous_phase)
-#     freq = np.fft.fftfreq(instantaneous_phase.shape[-1])
-#     sin = []
-#     for i in range(len(freq)):
-#         sin.append(amplitude_envelope[i] * np.sum((sp[-i]-sp[i]).imag/(2*T) * np.sin(2.*np.pi*freq[i] + instantaneous_phase[i])))
-#     return sin
+def inst_freq(p):
+    return np.diff(p) / (2.0*np.pi) * len(p)
 
 def hilbert_from_scratch(u):
     # N : fft length
@@ -69,22 +35,18 @@ def hilbert_from_scratch(u):
     return v
 
 def my_hilbert(inp):
-    analytic_signal = scipy.signal.hilbert(inp.cpu())
+    analytic_signal = scipy.signal.hilbert(inp)
     #analytic_signal = hilbert_from_scratch(inp)
-    amplitude_envelope = torch.from_numpy(np.abs(analytic_signal)).cuda()
-    instantaneous_phase = torch.from_numpy(np.unwrap(np.angle(analytic_signal))).cuda()
+    amplitude_envelope = np.abs(analytic_signal)
+    instantaneous_phase = np.unwrap(np.angle(analytic_signal))
     return amplitude_envelope, instantaneous_phase
 
 def inverse_hilbert(amplitude_envelope, instantaneous_phase):
     # close enough i guess
-    p = []
-    for t in instantaneous_phase:
-        p.append(t)
-    p = torch.stack(p)
-    freq = inst_freq(p)
+    freq = inst_freq(instantaneous_phase)
     #zero = torch.zeros_like(torch.tensor([1])).cuda()
     #freq = torch.cat((zero, freq), dim=0)
-    return freq * torch.tensor(amplitude_envelope).cuda()
+    return freq * amplitude_envelope
 
 def invert_hilb_tensor(t):
     return inverse_hilbert(t[0], t[1])
@@ -95,3 +57,32 @@ def prep_hilb_for_denses(t):
 def stack_hilb(t):
     return np.dstack((t.cpu()[0], t.cpu()[1]))
 
+class HilbertWithGradients(Function):
+    @staticmethod
+    def forward(ctx, s):
+        s_ = s.detach().cpu().numpy()
+        amp, phase = my_hilbert(s_)
+        return s.new((amp, phase))
+
+    @staticmethod
+    def backward(ctx, grad_output, n_fft):
+        if grad_output is None:
+            return None, None
+        numpy_go = grad_output.cpu().numpy()
+        amp, phase = my_hilbert(numpy_go)
+        return numpy_go.new((amp, phase))
+
+class InverseHilbertWithGradients(Function):
+    @staticmethod
+    def forward(ctx, s):
+        s_ = s.detach().cpu().numpy()
+        amp, phase = inverse_hilbert(s_)
+        return p.new((amp, phase))
+
+    @staticmethod
+    def backward(ctx, grad_output, n_fft):
+        if grad_output is None:
+            return None, None
+        numpy_go = grad_output.cpu().numpy()
+        amp, phase = inverse_hilbert(numpy_go)
+        return numpy_go.new((amp, phase))

tensaudio.py
@@ -2,9 +2,11 @@ import collections
 import os
 import time
 from datetime import datetime
+import curses
 
 import librosa
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 import scipy.io.wavfile
 import six
@@ -35,22 +37,39 @@ def record_amp_phase(amp, phase):
 def plot_metrics(i, show=False):
     timestamp = str(datetime.now().strftime("%d.%m.%Y_%H.%M.%S"))
     dirname = os.path.join(PLOTS_DIR, timestamp)
-
-    fig3 = plt.figure()
-    fig3.suptitle("Gen/Dis Losses " + timestamp)
-    plt.plot(range(len(total_gen_losses)), total_gen_losses, label="Gen Losses")
-    plt.plot(range(len(total_dis_losses)), total_dis_losses, label="Dis Losses")
-    plt.plot(range(len(total_real_verdicts)), total_real_verdicts, label="Real Verdicts")
-    plt.plot(range(len(total_fake_verdicts)), total_fake_verdicts, label="Fake Verdicts")
-    plt.legend()
     os.mkdir(dirname)
 
-    # filename1 = str('AMP_'+timestamp+'_'+str(i)+'.png')
-    # filename2 = str('PHASE_'+timestamp+'_'+str(i)+'.png')
+    filename1 = str('AMP_'+timestamp+'_'+str(i)+'.png')
+    filename2 = str('PHASE_'+timestamp+'_'+str(i)+'.png')
     filename3 = str('LOSSES_'+timestamp+'_'+str(i)+'.png')
-    # fig1.savefig(os.path.join(dirname, filename1))
-    # fig2.savefig(os.path.join(dirname, filename2))
-    fig3.savefig(os.path.join(dirname, filename3))
+
+    # if len(total_amps) > 1:
+    #     fig1 = plt.figure(figsize=(10,10))
+    #     fig1.suptitle("Amplitude Envelopes "+ timestamp)
+    #     ax1 = fig1.add_subplot(111, projection='3d')
+    #     Y, X = np.meshgrid(range(len(total_amps)), range(len(total_amps[0])))
+    #     Z = np.reshape(np.array(total_amps), X.shape)
+    #     ax1.scatter(X, Y, Z, s=1, cmap=cm.coolwarm)
+    #     fig1.savefig(os.path.join(dirname, filename1))
+
+    # if len(total_phases) > 1:
+    #     fig2 = plt.figure(figsize=(10,10))
+    #     fig2.suptitle("Instantaneous Phases "+ timestamp)
+    #     ax2 = fig2.add_subplot(111, projection='3d')
+    #     Y, X = np.meshgrid(range(len(total_phases)), range(len(total_phases[0])))
+    #     Z = np.reshape(np.array(total_phases), X.shape)
+    #     ax2.scatter(X, Y, Z, s=1, cmap=cm.coolwarm)
+    #     fig2.savefig(os.path.join(dirname, filename2))
+
+    if len(total_gen_losses) + len(total_dis_losses) + len(total_real_verdicts) + len(total_fake_verdicts) > 0:
+        fig3 = plt.figure(figsize=(10,10))
+        fig3.suptitle("Gen/Dis Losses " + timestamp)
+        plt.plot(range(len(total_gen_losses)), total_gen_losses, label="Gen Losses")
+        plt.plot(range(len(total_dis_losses)), total_dis_losses, label="Dis Losses")
+        plt.plot(range(len(total_real_verdicts)), total_real_verdicts, label="Real Verdicts")
+        plt.plot(range(len(total_fake_verdicts)), total_fake_verdicts, label="Fake Verdicts")
+        plt.legend()
+        fig3.savefig(os.path.join(dirname, filename3))
 
     plt.close()
 
@@ -154,10 +173,12 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
-def run_models(real):
+def run_models(window, real):
     global gen_loss, dis_loss
 
     def verdict_str(out):
+        if len(out) > 1:
+            out = float(torch.mean(out))
         if REAL_LABEL > FAKE_LABEL:
             if REAL_LABEL-out > FAKE_LABEL+out:
                 return "FAKE"
@@ -175,9 +196,11 @@ def run_models(real):
 
     dis.zero_grad()
     output = dis(real).view(-1)
-    print("| Real Verdict: ", verdict_str(output[0]), float(output[0]))
-    total_real_verdicts.append(float(output[0]))
-    b_size = real.size(0)
+    window.addstr(6,0, "| Real Verdict: "+verdict_str(output)+"\t"
+        + str(round(float(torch.mean(output)), 4))
+    )
+    total_real_verdicts.append(float(torch.mean(output)))
+    b_size = output.size(0)
     label = torch.full((b_size,), REAL_LABEL, dtype=torch.float).cuda()
     errD_real = dis.criterion(label, output)
     errD_real.backward()
@@ -195,32 +218,37 @@ def run_models(real):
 
     gen.zero_grad()
     output = dis(fake).view(-1)
-    print("| Fake Verdict: ", verdict_str(output[0]), float(output[0]))
-    total_fake_verdicts.append(float(output[0]))
+    window.addstr(7,0, "| Fake Verdict: "+verdict_str(output)+"\t"
+        + str(round(float(torch.mean(output)), 4))
+    )
+    total_fake_verdicts.append(float(torch.mean(output)))
+    label.fill_(REAL_LABEL)
     errG = gen.criterion(label, output)
     errG.backward()
     gen_loss = errG
     D_G_z2 = output.mean().item()
     gen_optim.step()
 
-def train_on_random(i, dirname):
+def train_on_random(window, i, dirname):
     _, z = create_inputs()
-    v_print("Passing training data to models.")
     begin_time = time.time()
-    run_models(z)
+    run_models(window, z)
     total_gen_losses.append(gen_loss)
     total_dis_losses.append(dis_loss)
-    print("|] Gen Loss:\t\t", round(float(gen_loss), 8))
-    print("|] Dis Loss:\t\t", round(float(dis_loss), 8))
+    window.addstr(8,0, "|] Gen Loss:\t\t"+ str(round(float(gen_loss), 4)))
+    window.addstr(9,0, "|] Dis Loss:\t\t"+ str(round(float(dis_loss), 4)))
 
     time_diff = time.time() - begin_time
-    print("Models successfully trained in", str(round(time_diff, ndigits=2)), "seconds.")
+    window.addstr(10,0, "Models successfully trained in "+ str(round(time_diff, ndigits=2))+ " seconds.")
     # if SAVE_MODEL_EVERY_ITERS > 0 and i % SAVE_MODEL_EVERY_ITERS == 0:
-    #     print("Saving checkpoints for models...")
+    #     cprint("Saving checkpoints for models...")
     #     torch.save(gen, os.path.join(MODEL_DIR, "gen_ckpts"))
     #     torch.save(dis, os.path.join(MODEL_DIR, "dis_ckpts"))
+    return time_diff
 
-def train_until_interrupt(save_plots=False):
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+
+def train_until_interrupt(window, save_plots=False):
     states = None
     i = 0
     j = 0
@@ -231,53 +259,68 @@ def train_until_interrupt(save_plots=False):
     time_since_last_save = 0
     time_last_saved = 0
     iters_per_sec = 1
+    secs_per_iter = 0
+    time_passed = 0
     timestamp = str(datetime.now().strftime("%d.%m.%Y_%H.%M.%S"))
     dirname = os.path.join(TRAINING_DIR, "training_"+timestamp)
-    print("="*80)
-    print("MODEL TRAINING BEGINS AT", timestamp)
-    print("="*80)
-    print()
+
+    clear()
+
+    window.addstr(0,0, "="*80)
+    window.addstr(1,0, "MODEL TRAINING BEGINS AT "+timestamp)
+    window.addstr(2,0, "="*80)
+
     if MAX_ITERS == 0:
         i = -1
     while i < MAX_ITERS:
+        if time_passed > RUN_FOR_SEC:
+            break
         try:
-            v_print("*"*40)
-            print("Initiating iteration #", j+1)
-            train_on_random(j, dirname)
+            window.addstr(4,0, "*"*40)
+            window.addstr(5,0, "Initiating iteration #"+str(j+1))
+            secs_per_iter = train_on_random(window, j, dirname)
             time_passed = time.time() - start_time
             diffs.append(time_passed - last_time)
             last_time = time_passed
             if len(diffs) > 2:
                 diffs = diffs[-2:]
-            print("Running time (seconds):", round(time_passed))
+            window.addstr(11,0, "Running time (seconds): "+ str(round(time_passed)))
             if len(diffs) > 0:
                 iters_per_sec = round(len(diffs) / sum(diffs))
-                print("Iterations/sec:", iters_per_sec)
+                window.addstr(12,0, "Iterations/sec:"+ str(iters_per_sec))
             avg_iters_per_sec = j / time_passed
             if SAVE_EVERY_SECONDS > 0 and avg_iters_per_sec > 0 and j / avg_iters_per_sec % float(SAVE_EVERY_SECONDS) < 1.0 / avg_iters_per_sec:
                 time_since_last_save = -1
-                print("Generating progress update...")
+                #cprint("Generating progress update...")
                 #time.sleep(0.5)
                 if num_times_saved == 0:
                     os.mkdir(dirname)
                 begin_time = time.time()
+                out1 = onestep.generate_one_step()
+                amp, phase = my_hilbert(out1.numpy())
+                if len(total_amps) <= num_times_saved:
+                    total_amps.append(amp)
+                if len(total_phases) <= num_times_saved:
+                    total_phases.append(phase)
                 if save_plots:
                     plot_metrics(j, show=False)
-                out1 = onestep.generate_one_step()
                 write_normalized_audio_to_disk(out1, dirname+'/progress'+str(j)+"_"+str(datetime.now().strftime("%d.%m.%Y_%H.%M.%S"))+'.wav')
                 time_last_saved = time.time()
                 time_diff = time.time() - begin_time
                 num_times_saved += 1
-                print("Progress update generated in", str(round(time_diff, ndigits=2)), "seconds.")
+                #cprint("Progress update generated in", str(round(time_diff, ndigits=2)), "seconds.")
             time_since_last_save = time.time() - time_last_saved
             if MAX_ITERS != 0:
                 i += 1
+            window.refresh()
+            if MAX_ITERS_PER_SEC > 0:
+                time.sleep((1/(MAX_ITERS_PER_SEC)) - time_passed % (1/(MAX_ITERS_PER_SEC)))
             if SLEEP_TIME > 0:
                 time.sleep(SLEEP_TIME)
             j += 1
         except KeyboardInterrupt:
             break
-    # print("Saving weights to disk...")
+    # cprint("Saving weights to disk...")
     # timestamp = str(datetime.now().strftime("%d.%m.%Y_%H.%M.%S"))
     # dirname = os.path.join(MODEL_DIR, "gen_weights", timestamp)
     # os.mkdir(dirname)
@@ -285,25 +328,26 @@ def train_until_interrupt(save_plots=False):
     # dirname = os.path.join(MODEL_DIR, "dis_weights", timestamp)
     # os.mkdir(dirname)
     # dis.save_weights(os.path.join(dirname, "dis_weights"))
-    print()
-    print("="*80)
-    print("MODEL TRAINING FINISHED AT", timestamp)
-    print("="*80)
+    window.addstr(14,0, "="*80)
+    window.addstr(15,0, "MODEL TRAINING FINISHED AT "+str(timestamp))
+    window.addstr(16,0, "="*80)
+    window.refresh()
+    time.sleep(1)
     return i
 
-v_print("Opening examples...")
+v_cprint("Opening examples...")
 EXAMPLE_FILES = open_truncate_pad(EXAMPLES_DIR)
-v_print("Opening example results...")
+v_cprint("Opening example results...")
 EXAMPLE_RESULT_FILES = open_truncate_pad(EXAMPLE_RESULTS_DIR)
-v_print("Opening inputs...")
+v_cprint("Opening inputs...")
 INPUT_FILES = open_truncate_pad(INPUTS_DIR)
-v_print("We have", len(INPUT_FILES), "inputs in the folder.")
+v_cprint("We have", len(INPUT_FILES), "inputs in the folder.")
 
 print("Resampling, stand by...")
 EXAMPLES = iterate_and_resample(EXAMPLE_FILES)
 INPUTS = iterate_and_resample(INPUT_FILES)
 EXAMPLE_RESULTS = iterate_and_resample(EXAMPLE_RESULT_FILES)
-v_print("Created", len(EXAMPLES), "Example Arrays and", len(EXAMPLE_RESULTS), "Example Result Arrays.")
+v_cprint("Created", len(EXAMPLES), "Example Arrays and", len(EXAMPLE_RESULTS), "Example Result Arrays.")
 
 gen = TA_Generator().cuda()
 dis = DPAM_Discriminator().cuda()
@@ -315,14 +359,16 @@ dis_optim = torch.optim.Adam(dis.parameters(), lr=DISCRIMINATOR_LR, betas=(DISCR
 onestep = OneStep(gen)
 
 if __name__ == "__main__":
-    i = train_until_interrupt()
-
+    i = curses.wrapper(train_until_interrupt, True)
     print("Generating...")
     if USE_REAL_AUDIO:
         data = onestep.generate_one_step()
     else:
         data = onestep.generate_one_step()
     print("Done!")
+    amp, phase = my_hilbert(data)
+    total_amps.append(amp)
+    total_phases.append(phase)
 
     write_normalized_audio_to_disk(data, 'out1.wav')
 