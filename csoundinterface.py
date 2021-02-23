Fixed gradients

csoundinterface.py
@@ -1,17 +1,32 @@
 import time
 import ctcsound
 import torch
+import torch.autograd as ag
+import torch.nn as nn
 import librosa
 from pythonosc.udp_client import SimpleUDPClient
 from global_constants import *
 
+class CSIRun(ag.Function):
+    @staticmethod
+    def forward(ctx, params):
+        params_ = params.detach().cpu().numpy()
+        ctx.save_for_backward(params)
+        ctx.mark_dirty(params)
+        return params.new(G_csi.perform(params_, None).to(torch.float)), params
+    @staticmethod
+    def backward(ctx, grad_output, dummy):
+        params = ctx.saved_tensors
+        return params
+
 class CsoundInterface():
-    def __init__(self, vis):
+    def __init__(self):
         self.param_len_in_seconds = OUTPUT_DURATION/TOTAL_PARAM_UPDATES
+        self.param_len_in_samples = PARAM_UPDATE_SAMPLES
         self.c = ctcsound.Csound()
 
         self.client = SimpleUDPClient("127.0.0.1", 7770)
-        self.vis = vis
+        #self.vis = vis
         self.total_instruments = 1
         self.total_gens = 1
         self.params = [1.] * N_PARAMS
@@ -32,7 +47,7 @@ class CsoundInterface():
     def param_gen(self, num):
         while True:
             if num in [1]:
-                yield np.log2(self.get_param(num))*5
+                yield torch.log2(self.get_param(num))*5
             else:
                 yield self.get_param(num)*10
 
@@ -51,10 +66,11 @@ class CsoundInterface():
         return 0
 
     def perform(self, params, window):
+        #out = torch.autograd.Variable(torch.empty(0), requires_grad=True).cuda()
         out = np.array([])
         done = False
-        last_params = params[0:N_PARAMS]
-        self.update_params(last_params)
+        #last_params = params[0:N_PARAMS]
+        #self.update_params(last_params)
 
         i = 1
         tim = ctcsound.RtClock()
@@ -63,15 +79,16 @@ class CsoundInterface():
         while not done:
             param_slice = params[(i-1)*N_PARAMS:(i)*N_PARAMS]
             if len(param_slice) == 0:
+                # this should never happen, but if it does, loop back to the start
+                raise RuntimeWarning("Warning: Length of parameter slice invalid")
                 i = 1
                 param_slice = params[0:N_PARAMS]
-            for param in param_slice:
-                if param < 0.1:
-                    i += 1
-                    continue
+            for j in range(len(param_slice)):
+                if param_slice[j] < 0.01:
+                    param_slice[j] = 0.01
             param_slice = self.scale_params(param_slice)
             self.update_params(param_slice)
-            """
+            
             if window is not None:
                 try:
                     window.addstr(14,0, "Current Parameters:")
@@ -88,69 +105,69 @@ class CsoundInterface():
                 except curses.error:
                     pass
                 try:
-                    window.addstr(15,0, str(round(self.params[0], 2))+"\t"+
-                                        str(round(self.params[1], 2))+"\t"+
-                                        str(round(self.params[2], 2))+"\t"+
-                                        str(round(self.params[3], 2))+"\t"+
-                                        str(round(self.params[4], 2))+"\t"+
-                                        str(round(self.params[5], 2))+"\t"+
-                                        str(round(self.params[6], 2))+"\t"+
-                                        str(round(self.params[7], 2))+"\t"+
-                                        str(round(self.params[8], 2))+"\t"+
-                                        str(round(self.params[9], 2))+"\t")
+                    window.addstr(15,0, str(round(float(self.params[0]), 2))+"\t"+
+                                        str(round(float(self.params[1]), 2))+"\t"+
+                                        str(round(float(self.params[2]), 2))+"\t"+
+                                        str(round(float(self.params[3]), 2))+"\t"+
+                                        str(round(float(self.params[4]), 2))+"\t"+
+                                        str(round(float(self.params[5]), 2))+"\t"+
+                                        str(round(float(self.params[6]), 2))+"\t"+
+                                        str(round(float(self.params[7]), 2))+"\t"+
+                                        str(round(float(self.params[8]), 2))+"\t"+
+                                        str(round(float(self.params[9]), 2))+"\t")
                 except curses.error:
                     pass
                 try:
-                    window.addstr(16,0, str(round(self.params[10], 2))+"\t"+
-                                        str(round(self.params[11], 2))+"\t"+
-                                        str(round(self.params[12], 2))+"\t"+
-                                        str(round(self.params[13], 2))+"\t"+
-                                        str(round(self.params[14], 2))+"\t"+
-                                        str(round(self.params[15], 2))+"\t"+
-                                        str(round(self.params[16], 2))+"\t"+
-                                        str(round(self.params[17], 2))+"\t"+
-                                        str(round(self.params[18], 2))+"\t"+
-                                        str(round(self.params[19], 2))+"\t")
+                    window.addstr(16,0, str(round(float(self.params[10]), 2))+"\t"+
+                                        str(round(float(self.params[11]), 2))+"\t"+
+                                        str(round(float(self.params[12]), 2))+"\t"+
+                                        str(round(float(self.params[13]), 2))+"\t"+
+                                        str(round(float(self.params[14]), 2))+"\t"+
+                                        str(round(float(self.params[15]), 2))+"\t"+
+                                        str(round(float(self.params[16]), 2))+"\t"+
+                                        str(round(float(self.params[17]), 2))+"\t"+
+                                        str(round(float(self.params[18]), 2))+"\t"+
+                                        str(round(float(self.params[19]), 2))+"\t")
                 except curses.error:
                     pass
                 try:
-                    window.addstr(17,0, str(round(self.params[20], 2))+"\t"+
-                                        str(round(self.params[21], 2))+"\t"+
-                                        str(round(self.params[22], 2))+"\t"+
-                                        str(round(self.params[23], 2))+"\t"+
-                                        str(round(self.params[24], 2))+"\t"+
-                                        str(round(self.params[25], 2))+"\t"+
-                                        str(round(self.params[26], 2))+"\t"+
-                                        str(round(self.params[27], 2))+"\t"+
-                                        str(round(self.params[28], 2))+"\t"+
-                                        str(round(self.params[29], 2))+"\t")
+                    window.addstr(17,0, str(round(float(self.params[20]), 2))+"\t"+
+                                        str(round(float(self.params[21]), 2))+"\t"+
+                                        str(round(float(self.params[22]), 2))+"\t"+
+                                        str(round(float(self.params[23]), 2))+"\t"+
+                                        str(round(float(self.params[24]), 2))+"\t"+
+                                        str(round(float(self.params[25]), 2))+"\t"+
+                                        str(round(float(self.params[26]), 2))+"\t"+
+                                        str(round(float(self.params[27]), 2))+"\t"+
+                                        str(round(float(self.params[28]), 2))+"\t"+
+                                        str(round(float(self.params[29]), 2))+"\t")
                 except curses.error:
                     pass
                 try:
-                    window.addstr(18,0, str(round(self.params[30], 2))+"\t"+
-                                        str(round(self.params[31], 2))+"\t"+
-                                        str(round(self.params[32], 2))+"\t"+
-                                        str(round(self.params[33], 2))+"\t"+
-                                        str(round(self.params[34], 2))+"\t"+
-                                        str(round(self.params[35], 2))+"\t"+
-                                        str(round(self.params[36], 2))+"\t"+
-                                        str(round(self.params[37], 2))+"\t"+
-                                        str(round(self.params[38], 2))+"\t"+
-                                        str(round(self.params[39], 2))+"\t")
+                    window.addstr(18,0, str(round(float(self.params[30]), 2))+"\t"+
+                                        str(round(float(self.params[31]), 2))+"\t"+
+                                        str(round(float(self.params[32]), 2))+"\t"+
+                                        str(round(float(self.params[33]), 2))+"\t"+
+                                        str(round(float(self.params[34]), 2))+"\t"+
+                                        str(round(float(self.params[35]), 2))+"\t"+
+                                        str(round(float(self.params[36]), 2))+"\t"+
+                                        str(round(float(self.params[37]), 2))+"\t"+
+                                        str(round(float(self.params[38]), 2))+"\t"+
+                                        str(round(float(self.params[39]), 2))+"\t")
                 except curses.error:
                     pass
                 try:
-                    window.addstr(19,0, str(round(self.params[40], 2))+"\t"+
-                                        str(round(self.params[41], 2))+"\t"+
-                                        str(round(self.params[42], 2))+"\t"+
-                                        str(round(self.params[43], 2))+"\t"+
-                                        str(round(self.params[44], 2))+"\t"+
-                                        str(round(self.params[45], 2))+"\t"+
-                                        str(round(self.params[46], 2))+"\t"+
-                                        str(round(self.params[47], 2))+"\t"+
-                                        str(round(self.params[48], 2))+"\t"+
-                                        str(round(self.params[49], 2))+"\t"+
-                                        str(round(self.params[50], 2))+"\t")
+                    window.addstr(19,0, str(round(float(self.params[40]), 2))+"\t"+
+                                        str(round(float(self.params[41]), 2))+"\t"+
+                                        str(round(float(self.params[42]), 2))+"\t"+
+                                        str(round(float(self.params[43]), 2))+"\t"+
+                                        str(round(float(self.params[44]), 2))+"\t"+
+                                        str(round(float(self.params[45]), 2))+"\t"+
+                                        str(round(float(self.params[46]), 2))+"\t"+
+                                        str(round(float(self.params[47]), 2))+"\t"+
+                                        str(round(float(self.params[48]), 2))+"\t"+
+                                        str(round(float(self.params[49]), 2))+"\t"+
+                                        str(round(float(self.params[50]), 2))+"\t")
                 except curses.error:
                     pass
 
@@ -160,21 +177,56 @@ class CsoundInterface():
                 except curses.error:
                     pass
 
-            """
+            
+            tim2 = ctcsound.RtClock()
+            tim2_cpu = self.c.CPUTime(tim2)
             done2 = False
+            out2 = np.array([])
+            k = 0
             while not done2:
                 res = 0
-                while not res:
-                    res = self.c.performBuffer()
-                    newtime = self.c.CPUTime(tim)
-                    if res > 0 or (newtime - tim_cpu) >= self.param_len_in_seconds:
-                        break
+                newtime = 0
+                m = 0
+                while res == 0:
+                    res = self.c.performKsmps()
+                    if res < 0:
+                        raise RuntimeError("performKsmps() failed!")
+                    newtime = self.c.CPUTime(tim2)
                     self.update_params(param_slice)
-                out = np.concatenate((out, self.c.outputBuffer()))
-                done2 = len(out) >= self.param_len_in_seconds*SAMPLE_RATE
+                    m += 1
+                    time.sleep(0.001) # not too fast, now
+                if window is not None:
+                    try:
+                        window.addstr(8,60, "m = "+ str(m)+"\t("+str(round(newtime-tim2_cpu, 4))+" sec)")
+                        window.refresh()
+                    except curses.error:
+                        pass
+                outbuf = self.c.outputBuffer()
+                first_zero_crossing = KONTROL_SAMPLES
+                #while first_zero_crossing <= len(outbuf) and outbuf[first_zero_crossing] >= 0.0001:
+                #    first_zero_crossing += 1
+                out2 = np.concatenate((out2, outbuf[KONTROL_SAMPLES:KONTROL_SAMPLES+PARAM_UPDATE_SAMPLES]))
+                #out.retain_grad()
+                done2 = len(out2) >= self.param_len_in_seconds*SAMPLE_RATE
+                k += 1
+            if window is not None:
+                try:
+                    window.addstr(9,60, "k = "+str(k))
+                    window.refresh()
+                except curses.error:
+                    pass
+            out = np.concatenate((out, out2))
             done = len(out) >= TOTAL_SAMPLES_OUT
             i += 1
-        return out
+        i -= 1
+        if window is not None:
+            try:
+                window.addstr(10,60, "i = "+ str(i))
+                window.refresh()
+            except curses.error:
+                pass
+        #print("Done generating audio in", i-1, "iters (out of target", TOTAL_PARAM_UPDATES, "iters)")
+        return torch.from_numpy(out[:TOTAL_SAMPLES_OUT]).flatten().cuda().requires_grad_(True)
 
     def scale_params(self, params):
         params = params.flatten()
@@ -195,8 +247,8 @@ class CsoundInterface():
         for i in range(N_PARAMS):
             #print(str(i)+": "+str(params[i]))
             self.set_param(i+1, params[i])
-        self.vis.update_vals()
-        self.vis.update_display()
+        #self.vis.update_vals()
+        #self.vis.update_display()
     
     def update_score(self):
         out = """\n
@@ -209,11 +261,14 @@ class CsoundInterface():
         out += "\n"
         return out
 
+global G_csi
+G_csi = CsoundInterface()
+
 FM_SYNTH = """
 instr 	1000
 	kPorttime	linseg	0,0.01,0.05		;PORTAMENTO TIME RAMPS UP QUICKLY FROM ZERO
 
-    
+
 
 """
 param_init_string = ""

discriminator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torch.fft
 import torch.nn as nn
 import torch.nn.functional as F
 from helper import *
@@ -16,11 +17,12 @@ class TADiscriminator(nn.Module):
 
         self.n_layers = N_DIS_LAYERS
         self.ksz = DIS_KERNEL_SIZE
-        self.ndf = 2
+        self.ndf = 1
 
         self.net = []
+        #self.net.append(nn.Identity())
         i = 0
-        for _ in range(self.n_layers):
+        for _ in range(self.n_layers//2):
             c = 2**i
             n = 2**(i+1)
             self.net.append(nn.Conv2d(self.ndf*c, self.ndf*n, self.ksz, 2, 1, bias=False))
@@ -29,35 +31,44 @@ class TADiscriminator(nn.Module):
             i += 1
 
         self.net.append(nn.Conv2d(self.ndf*(2**i), 1, self.ksz, 1, 0, bias=False))
-        self.net.append(nn.Sigmoid())
+        self.net.append(nn.Flatten())
 
-        self.net = nn.ModuleList(self.net)
+        linear_units = ((2*TOTAL_SAMPLES_OUT)+KONTROL_SAMPLES)//(2**i)
+        self.net2 = []
+        for _ in range(self.n_layers//2):
+            self.net2.append(nn.Linear(linear_units, linear_units))
+            self.net2.append(nn.LeakyReLU(0.2, inplace=True))
+        
+        self.net2.append(nn.Sigmoid())
+
+        self.net = nn.Sequential(*self.net)
+        self.net2 = nn.Sequential(*self.net2)
     
     def criterion(self, label, output):
-        l = self.loss(torch.unsqueeze(output, 0), torch.unsqueeze(label, 0))
-        l = F.relu(l)
+        l = self.loss(output, label.cpu())
+        #l = F.relu(l)
         return l
 
-    def forward(self, input1):
-        if type(input1) != torch.Tensor:
-            input1 = torch.from_numpy(input1).float().cuda()
+    def forward(self, inp):
         if DIS_MODE == 0:
-            amp, phase = HilbertWithGradients.apply(input1).detach().cuda()
-            hilb = torch.stack((amp, phase))
-            hilb = torch.unsqueeze(hilb, 0)
-            hilb = torch.unsqueeze(hilb, 3)
-            out = self.net[0](hilb)
+            amp, phase = HilbertWithGradients.apply(inp)
+            inp = torch.stack((amp, phase))
+            inp = torch.unsqueeze(inp, 0)
+            inp = torch.unsqueeze(inp, 3)
+            out = self.net[0](inp.to(torch.float))
         elif DIS_MODE == 1:
-            real, imag = FFTWithGradients.apply(input1).detach().cuda()
-            stft1 = torch.stack((real, imag))
-            stft1 = torch.unsqueeze(stft1, 0)
-            stft1 = torch.unsqueeze(stft1, 3)
-            out = self.net[0](stft1)
-        
-        i = 1
-        for layer in self.net[1:]:
-            #cprint("In discriminator layer #", i)
-            out = layer(out)
-            i += 1
-        #out = torch.mean(out)
-        return out
+            #mag, phas = FFTWithGradients.apply(input1)
+            fft1 = torch.fft.fft(inp)
+            real, imag = torch.real(fft1), torch.imag(fft1)
+            real.retain_grad()
+            imag.retain_grad()
+            mag = torch.sqrt(real**2 + imag**2)
+            pha = torch.atan2(real, imag)
+            mag.retain_grad()
+            pha.retain_grad()
+            stacked = torch.stack([mag, pha]).cpu()
+            stacked.retain_grad()
+
+        out1 = self.net.forward(torch.unsqueeze(torch.unsqueeze(stacked, 0), 0).to(torch.float))
+        out2 = self.net2.forward(out1)
+        return out2

generator.py
@@ -25,7 +25,7 @@ class TAInstParamGenerator(nn.Module):
 
         self.n_layers = N_GEN_LAYERS
         self.ksz = GEN_KERNEL_SIZE
-        self.ndf = N_PARAMS * (5+TOTAL_PARAM_UPDATES)
+        self.ndf = N_PARAMS * 2 * TOTAL_PARAM_UPDATES # ensure we never run out of params
 
         self.layers = [nn.ConvTranspose1d(TOTAL_SAMPLES_IN, self.ndf, self.ksz)]
         i = 0
@@ -40,30 +40,18 @@ class TAInstParamGenerator(nn.Module):
             i += 1
 
         #self.layers.append(nn.Conv1d(self.ndf, self.ndf//2, self.ksz, 1, 0, bias=False))
-        self.layers.append(nn.Sigmoid())
+        #self.layers.append(nn.Sigmoid())
 
-        self.layers = nn.ModuleList(self.layers)
+        self.layers = nn.Sequential(*self.layers)
     
     def criterion(self, label, output):
-        l = self.loss(torch.unsqueeze(output, 0), torch.unsqueeze(label, 0))
-        l = F.relu(l)
+        l = self.loss(output.cuda(), label)
+        #l = F.relu(l)
         return l
     
     def forward(self, inp):
-        #print(inp.shape)
-        out = self.layers[0](inp)
-        i = 1
-        for layer in self.layers[1:]:
-            out = layer(out)
-            i += 1
-        out = out.view(out.size(0), -1)
-        out -= out.min(1, keepdim=True)[0]
-        out /= out.max(1, keepdim=True)[0]
-        out = out.view(self.ndf)
-        out = out[N_PARAMS*5:] # for some reason the first few sets of params are always 0,
-                             # so we generate 5 more than we need and start at the 6th
-        #print(out.shape)
-        return out
+        a = self.layers.forward(inp).flatten()
+        return a/torch.max(torch.abs(a))
 
 class TAGenerator(nn.Module):
     def __init__(self, **kwargs):

helper.py
@@ -369,9 +369,9 @@ def prep_data_for_batch_operation(t, exp_batches, exp_channels, exp_timesteps, g
       return torch.reshape(torch.as_tensor(t).cuda(), (b, c, s))
   #return tf.reshape(t, (N_BATCHES, 2, 1))
 def normalize_audio(a):
-  return a/np.max(np.abs(a))
+  return a/torch.max(torch.abs(a))
 def write_normalized_audio_to_disk(a, fn):
-  scaled = np.int16(normalize_audio(a) * 32767)
+  scaled = np.int16(normalize_audio(a).detach().cpu().numpy() * 32767)
   soundfile.write(fn, scaled, SAMPLE_RATE, SUBTYPE)
 
 class FFTWithGradients(Function):
@@ -379,7 +379,13 @@ class FFTWithGradients(Function):
     def forward(ctx, p):
         p_ = p.detach().cpu().numpy()
         result = np.fft.fft(p_.astype('float32'))
-        return p.new((np.real(result), np.imag(result)))
+        real = np.real(result)
+        ctx.save_for_backward(real)
+        imag = np.imag(result)
+        ctx.save_for_backward(imag)
+        mag = np.sqrt(real**2 + imag**2)
+        phas = np.arctan2(real, imag)
+        return p.new((mag, phas))
 
     @staticmethod
     def backward(ctx, grad_output):
@@ -388,10 +394,10 @@ class FFTWithGradients(Function):
         numpy_go = grad_output.cpu().numpy()
         #print(numpy_go.shape)
         data = []
-        for i in range(len(numpy_go[0])):
+        for i in range(len(ctx.real)):
           d = []
-          for j in range(len(numpy_go[0][i])):
-            d.append(np.complex(numpy_go[0][i][j], numpy_go[1][i][j]))
+          for j in range(len(ctx.real[i])):
+            d.append(np.complex(ctx.real[i][j], ctx.imag[i][j]))
           data.append(d)
         data = np.array(data)
         orig_size = data.size//2

pgvis.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 import pygame
 from pygame.locals import *
@@ -73,25 +74,31 @@ class PGVisualizer():
             self.val_history = self.val_history[dif:self.max_vals]
             self.update_display()
 
+    def draw_metrics(self, img, size):
+        surf = pygame.image.fromstring(img, size, "RGB")
+        self.surface.blit(surf, (0,0))
+        pygame.display.flip()
+
     def update_display(self):
         self.surface.fill((50,50,50))
         
         if len(self.val_history) > 5:
             x = 0
-            points = []
+            points = torch.Tensor([])
             for iteration in self.val_history:
                 this_iter = []
                 for val in iteration:
                     this_iter.append([x, (val*2)+100])
-                points.append(this_iter)
+                points = torch.cat((points, torch.Tensor(this_iter)))
                 x += self.width/len(self.val_history)
-            points = np.array(points)
-            points = np.transpose(points, [1,0,2])
+            points = points.permute(1, 0, 2)
             i = 0
             for line in points:
                 pgline = []
                 for point in line:
-                    pgline.append(self.to_pygame_coords(point[0], point[1]))
+                    x = point[0].item()
+                    y = point[1].item()
+                    pgline.append(self.to_pygame_coords(x, y))
                 pygame.draw.lines(self.surface, self.colors[i], False, pgline, 2)
                 i += 1
             

tensaudio.py
@@ -9,6 +9,7 @@ import librosa
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib import animation
+import matplotlib.backends.backend_agg as agg
 import numpy as np
 import scipy.io.wavfile
 import six
@@ -17,12 +18,14 @@ import torch
 import torch.nn as nn
 
 from pgvis import init_gvis
-from csoundinterface import CsoundInterface
+from csoundinterface import CsoundInterface, CSIRun, G_csi
 from discriminator import TADiscriminator
 from generator import TAGenerator, TAInstParamGenerator
 from global_constants import *
 from helper import *
 from hilbert import *
+
+torch.autograd.set_detect_anomaly(True)
 
 np.random.seed(int(round(time.time())))
 torch.random.seed()
@@ -52,11 +55,10 @@ def record_amp_phase(amp, phase):
     # total_amps.append(amp)
     # total_phases.append(phase)
     pass
-def plot_metrics(i, show=False):
+def plot_metrics(i, save_to_disk=False):
     global total_gen_losses, total_dis_losses
     timestamp = str(datetime.now().strftime("%d.%m.%Y_%H.%M.%S"))
     dirname = os.path.join(PLOTS_DIR, timestamp)
-    os.mkdir(dirname)
 
     filename1 = str('AMP_'+timestamp+'_'+str(i)+'.png')
     filename2 = str('PHASE_'+timestamp+'_'+str(i)+'.png')
@@ -85,34 +87,50 @@ def plot_metrics(i, show=False):
     if type(total_dis_losses) is torch.Tensor:
         total_dis_losses = total_dis_losses.detach().cpu().numpy()
     if len(total_gen_losses) + len(total_dis_losses) + len(total_real_verdicts) + len(total_fake_verdicts) > 0:
-        fig3 = plt.figure(figsize=(10,10))
+        fig3, (ax1, ax2, ax3) = plt.subplots(3,1, figsize=[8,6], dpi=100)
+        fig3.subplots_adjust(hspace=1)
         fig3.suptitle("Gen/Dis Losses " + timestamp)
-        plt.plot(range(len(total_gen_losses)), total_gen_losses, label="Gen Losses")
-        plt.plot(range(len(total_dis_losses)), total_dis_losses, label="Dis Losses")
-        plt.plot(range(len(total_real_verdicts)), total_real_verdicts, label="Real Verdicts")
-        plt.plot(range(len(total_fake_verdicts)), total_fake_verdicts, label="Fake Verdicts")
-        plt.legend()
-        fig3.savefig(os.path.join(dirname, filename3))
-
+        ax1.set_title("Gen Losses")
+        ax1.plot(range(len(total_gen_losses)), total_gen_losses, color="b")
+        ax2.set_title("Dis Losses")
+        ax2.plot(range(len(total_dis_losses)), total_dis_losses, color="r")
+        ax3.set_title("Real/Fake Verdicts")
+        ax3.plot(range(len(total_real_verdicts)), total_real_verdicts, label="Real", color="g")
+        ax3.plot(range(len(total_fake_verdicts)), total_fake_verdicts, label="Fake", color="m")
+        ax3.legend()
+        canvas = agg.FigureCanvasAgg(fig3)
+        canvas.draw()
+        renderer = canvas.get_renderer()
+        raw_data = renderer.tostring_rgb()
+        if save_to_disk and i % SAVE_EVERY_SECONDS == 0:
+            os.mkdir(dirname)
+            fig3.savefig(os.path.join(dirname, filename3))
+    size = canvas.get_width_height()
     plt.close()
+    return raw_data, size
 
 def open_truncate_pad(name):
     ret = []
+    srs = torch.empty(0)
     for filename in os.listdir(os.path.join(RESOURCES_DIR, name)):
         f = soundfile.SoundFile(os.path.join(RESOURCES_DIR, name, filename), "r")
-        ret.append((f.read(), f.samplerate))
-    return ret
+        data = torch.as_tensor(f.read()).requires_grad_(True)
+        sr = torch.as_tensor(f.samplerate).unsqueeze(0)
+        #stacked = torch.stack((data, sr)).requires_grad_(True)
+        ret.append(data)
+        srs = torch.cat((srs.detach().requires_grad_(True), sr))
+    return ret, srs
 
-def iterate_and_resample(files):
+def iterate_and_resample(files, srs):
     arr = []
     for i in range(len(files)):
-        frick = files[i][0]
-        if len(frick.shape) > 1 and frick.shape[1] != 1:
-            frick = np.transpose(frick)[0]
-        frick = np.asfortranarray(frick)
-        a = librosa.resample(frick, files[i][1], SAMPLE_RATE)
-
-        arr.append(a.tolist())
+        fil = files[i]
+        if len(fil.shape) > 1 and fil.shape[1] != 1:
+            fil = fil.permute(1,0)[0]
+        fortfil = np.asfortranarray(fil.detach().cpu().numpy())
+        a = librosa.resample(fortfil, srs[i].item(), SAMPLE_RATE)
+        a = torch.from_numpy(a).requires_grad_(True)
+        arr.append(a)
     return arr
 
 
@@ -170,7 +188,7 @@ class OneStep():
 
     def generate_one_step(self):
         with torch.no_grad():
-            predicted_logits = self.generator(generate_input_noise(TOTAL_SAMPLES_IN)).detach().cpu()
+            predicted_logits = self.generator(generate_input_noise(TOTAL_SAMPLES_IN))
         return predicted_logits
 
 def create_input():
@@ -183,9 +201,9 @@ def create_input():
 
 def generate_input_noise(b_size):
     if GEN_MODE in [5]:
-        return torch.randn(1, TOTAL_SAMPLES_IN, 1).cuda()
+        return torch.randn(1, TOTAL_SAMPLES_IN, 1, requires_grad=True).cuda()
     else:
-        return torch.randn(b_size, TOTAL_SAMPLES_IN, 1, 1).cuda()
+        return torch.randn(b_size, TOTAL_SAMPLES_IN, 1, 1, requires_grad=True).cuda()
 
 gen_loss, dis_loss = None, None
 
@@ -199,24 +217,22 @@ def weights_init(m):
 
 def get_output_from_params(params, window):
     global current_params
-    params = params.clone().detach().cpu().numpy()
+    #params = params.clone().cpu().numpy()
     current_params = params
     #if window is not None:
         #window.addstr(3,0, str(params.shape[0]))
         #window.refresh()
-    audio = csi.perform(params, window)
+    audio, _ = CSIRun.apply(params.clone())
     #noise = np.random.randn(len(audio)) * 0.0001
-    for i in range(len(audio)):
-        if audio[i] == np.inf or audio[i] == np.nan:
-            audio[i] = 0.00001
+    # for i in range(len(audio)):
+    #     if audio[i] == np.inf or audio[i] == np.nan:
+    #         audio[i] = 0.00001
     return audio
 
 def run_models(window, real):
-    global gen_loss, dis_loss
+    #global gen_loss, dis_loss
 
     def verdict_str(out):
-        if len(out) > 1:
-            out = float(torch.mean(out))
         if REAL_LABEL > FAKE_LABEL:
             if REAL_LABEL-out > FAKE_LABEL+out:
                 return "FAKE"
@@ -232,59 +248,90 @@ def run_models(window, real):
             else:
                 return "UNSURE"
 
+    #real_label = torch.as_tensor(REAL_LABEL).to(torch.float).cuda()
+    #fake_label = torch.as_tensor(FAKE_LABEL).to(torch.float).cuda()
+
     dis.zero_grad()
-    output = dis(real).view(-1)
-    window.addstr(6,0, "| Real Verdict: "+verdict_str(output)+"\t"
-        + str(round(float(torch.mean(output)), 4))
-    )
-    total_real_verdicts.append(float(torch.mean(output)))
-    b_size = output.size(0)
-    label = torch.full((b_size,), REAL_LABEL, dtype=torch.float).cuda()
-    errD_real = dis.criterion(label, output)
-    errD_real.backward()
-    D_x = output.mean().item()
+
+    y = dis(real)
+    
+    real_verdict = y.mean()
+    label = torch.full(y.shape, REAL_LABEL).to(torch.float).cpu()
+    dis_loss_real = dis.criterion(label, y)
+    #dis_loss_real.backward(retain_graph=True)
 
     if GEN_MODE in [5]:
-        noise = generate_input_noise(TOTAL_SAMPLES_IN)
-        params = gen(noise).flatten()
+        noise = generate_input_noise(1)
+        params = gen(noise)
         audio = get_output_from_params(params, window)
-        fake = torch.from_numpy(audio).float().cuda()
+        fake = audio.float().cuda()
     else:
         noise = generate_input_noise(TOTAL_SAMPLES_IN)
         fake = gen(noise, window)
-    output = dis(fake).view(-1)
-    b_size = output.size(0)
-    label = torch.full((b_size,), FAKE_LABEL, dtype=torch.float).cuda()
-    errD_fake = dis.criterion(label, output)
-    errD_fake.backward()
-    D_G_z1 = output.mean().item()
-    dis_loss = errD_real + errD_fake
+    y = dis(fake)
+    
+    fake_verdict = y.mean()
+    label = torch.full(y.shape, FAKE_LABEL).to(torch.float).cpu()
+    dis_loss_fake = dis.criterion(label, y)
+    #dis_loss_fake.backward(retain_graph=True)
+    dis_loss = dis_loss_real + dis_loss_fake
+    dis_loss.backward(retain_graph=True)
     dis_optim.step()
-
+    
     gen.zero_grad()
-    output = dis(fake).view(-1)
-    window.addstr(7,0, "| Fake Verdict: "+verdict_str(output)+"\t"
-        + str(round(float(torch.mean(output)), 4))
-    )
-    total_fake_verdicts.append(float(torch.mean(output)))
-    b_size = output.size(0)
-    label = torch.full((b_size,), REAL_LABEL, dtype=torch.float).cuda()
-    errG = gen.criterion(label, output)
-    errG.backward()
-    gen_loss = errG
-    D_G_z2 = output.mean().item()
+
+    y = dis(fake)
+    fake_verdict = y.mean()
+    label = torch.full(y.shape, REAL_LABEL).to(torch.float).cuda()
+    gen_loss = gen.criterion(label, y)
+    #gradients = torch.autograd.grad(outputs=fake_verdict, inputs=gen_loss, grad_outputs=real_label, create_graph=False, retain_graph=None, only_inputs=True)[0]
+    #gp = ((gradients.norm(dim=1)-1)**2).mean() + fake_verdict
+    gen_loss.backward()
+    #gp.backward()
     gen_optim.step()
 
+    #dis_params = str([p.grad for p in list(dis.parameters())])
+    #gen_params = str([p.grad for p in list(gen.parameters())])
+    #print(gen_params)
+    #print("+++++++++++++++++++++++++++++++++++++++++")
+    #print(dis_params)
+    
+
+    try:
+        window.move(6,0)
+        window.clrtoeol()
+        window.move(7,0)
+        window.clrtoeol()
+        window.addstr(6,0, "| Real Verdict: "+verdict_str(real_verdict.item())+"\t"
+            + str(round(real_verdict.item(), 4)))
+        window.addstr(7,0, "| Fake Verdict: "+verdict_str(fake_verdict.item())+"\t"
+            + str(round(fake_verdict.item(), 4)))
+        window.refresh()
+    except curses.error:
+        pass
+
+
+    total_real_verdicts.append(real_verdict.item())
+    total_fake_verdicts.append(fake_verdict.item())
+
+    return gen_loss, dis_loss
+
 def train_on_random(window, i, dirname):
-    z = create_input()
+    z = torch.autograd.Variable(torch.as_tensor(create_input()), requires_grad=True).cuda()
     begin_time = time.time()
-    run_models(window, z)
+    gen_loss, dis_loss = run_models(window, z)
     total_gen_losses.append(gen_loss.clone().detach().cpu().numpy())
     total_dis_losses.append(dis_loss.clone().detach().cpu().numpy())
+    window.move(8,0)
+    window.clrtoeol()
+    window.move(9,0)
+    window.clrtoeol()
     window.addstr(8,0, "|] Gen Loss:\t\t"+ str(round(float(gen_loss), 4)))
     window.addstr(9,0, "|] Dis Loss:\t\t"+ str(round(float(dis_loss), 4)))
 
     time_diff = time.time() - begin_time
+    window.move(10,0)
+    window.clrtoeol()
     window.addstr(10,0, "Time per iteration: "+ str(round(time_diff, ndigits=2))+ " seconds.")
     window.refresh()
     # if SAVE_MODEL_EVERY_ITERS > 0 and i % SAVE_MODEL_EVERY_ITERS == 0:
@@ -328,6 +375,10 @@ def train_until_interrupt(window, save_plots=False):
             G_vis.handle_events()
             window.addstr(4,0, "*"*40)
             window.addstr(5,0, "Initiating iteration #"+str(j+1))
+            
+            #dis_optim.zero_grad()
+            #gen_optim.zero_grad()
+
             secs_per_iter = train_on_random(window, j, dirname)
             time_passed = time.time() - start_time
             diffs.append(time_passed - last_time)
@@ -340,12 +391,18 @@ def train_until_interrupt(window, save_plots=False):
             except curses.error:
                 pass
             if len(diffs) > 0:
-                iters_per_sec = round(len(diffs) / sum(diffs))
+                iters_per_sec = round(len(diffs) / sum(diffs), 3)
                 try:
+                    window.move(12,0)
+                    window.clrtoeol()
                     window.addstr(12,0, "Iterations/sec:"+ str(iters_per_sec))
+                    window.refresh()
                 except curses.error:
                     pass
             avg_iters_per_sec = j / time_passed
+            if save_plots:
+                img, size = plot_metrics(j, save_to_disk=False)
+                G_vis.draw_metrics(img, size)
             if SAVE_EVERY_SECONDS > 0 and avg_iters_per_sec > 0 and j / avg_iters_per_sec % float(SAVE_EVERY_SECONDS) < 1.0 / avg_iters_per_sec:
                 time_since_last_save = -1
                 #cprint("Generating progress update...")
@@ -353,19 +410,19 @@ def train_until_interrupt(window, save_plots=False):
                 if num_times_saved == 0:
                     os.mkdir(dirname)
                 begin_time = time.time()
+                if save_plots:
+                    img, size = plot_metrics(j, save_to_disk=True)
                 if GEN_MODE in [5]:
                     params = onestep.generate_one_step()
                     out1 = get_output_from_params(params, window)
-                    amp, phase = my_hilbert(out1)
+                    #amp, phase = my_hilbert(out1)
                 else:
-                    out1 = onestep.generate_one_step().numpy()
-                    amp, phase = my_hilbert(out1)
-                if len(total_amps) <= num_times_saved:
-                    total_amps.append(amp)
-                if len(total_phases) <= num_times_saved:
-                    total_phases.append(phase)
-                if save_plots:
-                    plot_metrics(j, show=False)
+                    out1 = onestep.generate_one_step()
+                    #amp, phase = my_hilbert(out1)
+                #if len(total_amps) <= num_times_saved:
+                #    total_amps.append(amp)
+                #if len(total_phases) <= num_times_saved:
+                #    total_phases.append(phase)
                 write_normalized_audio_to_disk(out1, dirname+'/progress'+str(j)+"_"+str(datetime.now().strftime("%d.%m.%Y_%H.%M.%S"))+'.wav')
                 time_last_saved = time.time()
                 time_diff = time.time() - begin_time
@@ -406,7 +463,7 @@ if __name__ == "__main__":
     global EXAMPLE_RESULTS, EXAMPLES, INPUTS
     global gen, dis, gen_optim, dis_optim, onestep
     print("Opening example results...")
-    EXAMPLE_RESULT_FILES = open_truncate_pad(EXAMPLE_RESULTS_DIR)
+    EXAMPLE_RESULT_FILES, EXAMPLE_RESULT_SRS = open_truncate_pad(EXAMPLE_RESULTS_DIR)
     if USE_REAL_AUDIO:
         print("Opening examples...")
         EXAMPLE_FILES = open_truncate_pad(EXAMPLES_DIR)
@@ -415,7 +472,7 @@ if __name__ == "__main__":
         print("We have", len(INPUT_FILES), "inputs in the folder.")
 
     print("Resampling, stand by...")
-    EXAMPLE_RESULTS = iterate_and_resample(EXAMPLE_RESULT_FILES)
+    EXAMPLE_RESULTS = iterate_and_resample(EXAMPLE_RESULT_FILES, EXAMPLE_RESULT_SRS)
     if USE_REAL_AUDIO:
         EXAMPLES = iterate_and_resample(EXAMPLE_FILES)
         INPUTS = iterate_and_resample(INPUT_FILES)
@@ -426,7 +483,7 @@ if __name__ == "__main__":
 
     print("Creating networks...")
     gen = TAInstParamGenerator().cuda()
-    dis = TADiscriminator().cuda()
+    dis = TADiscriminator().cpu()
     gen.apply(weights_init)
     dis.apply(weights_init)
     gen_optim = torch.optim.Adam(gen.parameters(), lr=GENERATOR_LR, betas=(GENERATOR_BETA, 0.999))
@@ -439,20 +496,18 @@ if __name__ == "__main__":
     if GEN_MODE in [5]:
         print("Initializing Visualizer...")
         G_vis = init_gvis(800, 600)
-        
 
         print("Creating CSound Interface...")
-        csi = CsoundInterface(G_vis)
-        result = csi.compile()
+        result = G_csi.compile()
         if result != 0:
             raise RuntimeError("CSound compilation failed!")
-        G_vis.watch_val(csi.param_gen(1))
+        #G_vis.watch_val(csi.param_gen(1))
         #for i in range(10):
         #    G_vis.watch_val(csi.param_gen(40+i))
         current_params = [0.]*N_PARAMS*TOTAL_PARAM_UPDATES
 
-    print("Initialization complete! Starting in 10 seconds...")
-    time.sleep(10)
+    print("Initialization complete! Starting...")
+    time.sleep(1)
 
     i = curses.wrapper(train_until_interrupt, True)
     print("Generating...")
@@ -462,11 +517,11 @@ if __name__ == "__main__":
     else:
         data = onestep.generate_one_step()
     print("Done!")
-    csi.stop()
+    G_csi.stop()
     amp, phase = my_hilbert(data)
     total_amps.append(amp)
     total_phases.append(phase)
 
     write_normalized_audio_to_disk(data, 'out1.wav')
 
-    plot_metrics(i)
+    plot_metrics(i, save_to_disk=True)